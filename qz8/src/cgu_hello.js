e0bc7de146cec54afa29bbe0cc76e74d950c3e4f
Brian H <tengeffort@gmail.com>
2022-05-17 23:49:56 +0800

    button

---
+++ qz8/src/cgu_hello.js
@@ -0,0 +1,8 @@
+const styleArgument = {fontSize:'100px', color:'red'};
+
+const HelloCGU = () => {
+    var output = <h1 style = {styleArgument}>hello CGU!!</h1>
+    return output;
+}
+
+export default HelloCGU;
