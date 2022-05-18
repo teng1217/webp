7b75b438c31f1d7ec0bd91c91b873066aee27180
Brian H <tengeffort@gmail.com>
2022-05-18 17:02:26 +0800

    hw

--- hw8/src/App.js
+++ hw8/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import login from './component/login';
 import username from './component/username';
 import password from './component/password';
-import button from './component/button';
+import SignIn from './SignIn';
 
 function App() {
   return (
     <div className = "App">
-        <div> {login()} </div>
+        {/* <div> {login()} </div>
         <div> {username()} </div>
         <div> {password()} </div>
-        <div> {button()} </div>
+        <div> {button()} </div> */
+        <div> {SignIn()} </div>}
+        
       </div>
   );
 }
